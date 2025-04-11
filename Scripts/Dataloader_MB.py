e53d3a34b645f14cc8b2b8632e5f82bdc7fa53e4
Marcin <marcinbaklodz@gmail.com>
2025-04-11 20:06:53 +0200

    data_from_samples

--- Scripts/Dataloader_MB.py
+++ Scripts/Dataloader_MB.py
@@ -14,22 +14,38 @@ class DataLoaderType(enum.Enum):
     POCKET_OR_WRIST = 4
 
 class CustomDataset(Dataset):
-    def __init__(self, description_file_path:str, data_directory_path:str, random_state:int = 0, mode:DataLoaderType = DataLoaderType.POCKET_AND_WRIST, debug:bool = True):
+    def __init__(self, description_file_path:str, data_directory_path:str, data_from_samples_ratio:int, data_lenght:int, random_state:int = 0, mode:DataLoaderType = DataLoaderType.POCKET_AND_WRIST, debug:bool = True):
         self.files_to_read_list:list = ["acce", "game_rv", "gyro"]#["acce", "game_rv", "gyro", "gyro_bias", "linacce", "magnet", "magnet_bias", "pressure", "rv"]
         self.debug = debug
+        self.data_from_samples_ratio = data_from_samples_ratio
+        self.data_lenght = data_lenght
+        self.mapping = {
+            None: 0,
+            'none': 0,
+            'green': 1,
+            'black': 2,
+            'red': 3,
+            'orange': 4
+        }
+        
         
         if random_state == 0: random_state = random.randint(0, 100)
         self.sample_names_array:np.ndarray = self.read_description_file(description_file_path)
         self.train_file_name, self.val_file_name, self.test_file_name = self.shufle_and_split_samples(self.sample_names_array, random_state = random_state)
         
-        self.train_sensor_reads = self.read_all_samples(self.train_file_name, data_directory_path, mode, "train")
-        self.val_sensor_reads = self.read_all_samples(self.val_file_name, data_directory_path, mode, "val")
-        self.test_sensor_reads = self.read_all_samples(self.test_file_name, data_directory_path, mode, "test")
+        self.train_sensor_reads_with_labels = self.read_all_samples(self.train_file_name, data_directory_path, mode, "train")
+        self.val_sensor_reads_with_labels = self.read_all_samples(self.val_file_name, data_directory_path, mode, "val")
+        self.test_sensor_reads_with_labels = self.read_all_samples(self.test_file_name, data_directory_path, mode, "test")
         
-    def read_all_samples(self, samples_array:np.ndarray, basic_directory:str, mode:DataLoaderType = DataLoaderType.POCKET_AND_WRIST, name:str = "default name")-> np.ndarray:
+        self.train_data, self.train_labels = self.convert_samples_to_data(self.train_sensor_reads_with_labels, self.data_lenght)
+        print(self.train_data.shape)
+        print(self.train_labels.shape)
+        
+    def read_all_samples(self, samples_array:np.ndarray, basic_directory:str, mode:DataLoaderType = DataLoaderType.POCKET_AND_WRIST, name:str = "default name")-> tuple:
         result = []
+        result_label = []
         choosed_by_mode_samples_array = np.array([])
-        if mode == DataLoaderType.POCKET:
+        if mode == DataLoaderType.WRIST:
             choosed_by_mode_samples_array = samples_array[:, [1, 3, 4, 5]]
             for sample, label, start, stop in choosed_by_mode_samples_array:
                 sample_path = os.path.join(basic_directory, "Telefon_na_rece", sample)
@@ -37,19 +53,35 @@ class CustomDataset(Dataset):
                 all_sensors_read = self.read_all_sensors(sample_path, int(start), int(stop))
                 if all_sensors_read.numel() > 0:  # Make sure it's not empty
                     result.append(all_sensors_read)
+                    result_label.append(label)
             print(mode)
-        elif mode == DataLoaderType.WRIST:
-            choosed_by_mode_samples_array = samples_array[:, [1, 3, 4, 5]]
+        elif mode == DataLoaderType.POCKET:
+            choosed_by_mode_samples_array = samples_array[:, [2, 3, 4, 5]]
             for sample, label, start, stop in choosed_by_mode_samples_array:
-                sample_path = os.path.join(basic_directory, "Telefon_na_rece", sample)
+                sample_path = os.path.join(basic_directory, "Telefon_w_kieszeni", sample)
                 if self.debug: print(sample_path, " ", label) 
                 all_sensors_read = self.read_all_sensors(sample_path, int(start), int(stop))
                 if all_sensors_read.numel() > 0:  # Make sure it's not empty
                     result.append(all_sensors_read)
+                    result_label.append(label)
             print(mode)
         elif mode == DataLoaderType.POCKET_AND_WRIST or mode == DataLoaderType.POCKET_OR_WRIST:
             choosed_by_mode_samples_array = samples_array[:, 1:6]
-            print(choosed_by_mode_samples_array)
+            for sample1, sample2, label, start, stop in choosed_by_mode_samples_array:
+                sample_path1 = os.path.join(basic_directory, "Telefon_na_rece", sample1)
+                sample_path2 = os.path.join(basic_directory, "Telefon_w_kieszeni", sample2)
+                if self.debug: print(sample_path1, " ", sample_path2, " ", label) 
+                all_sensors_read1 = self.read_all_sensors(sample_path1, int(start), int(stop))
+                all_sensors_read2 = self.read_all_sensors(sample_path2, int(start), int(stop))
+                
+                if all_sensors_read1.numel() > 0 and all_sensors_read2.numel() > 0:  # Make sure it's not empty
+                    if mode == DataLoaderType.POCKET_OR_WRIST:
+                        result.append(all_sensors_read1)
+                        result.append(all_sensors_read2)
+                        result_label.append(label)
+                        result_label.append(label)
+                    if mode == DataLoaderType.POCKET_AND_WRIST:
+                        raise Exception("This mode is not implemented yet. Due to different start moments for wrist and pocket")
             print(mode)
             
         if result:
@@ -58,9 +90,8 @@ class CustomDataset(Dataset):
             result = torch.empty(0)
             print("[{name}] No valid data found across sessions.")
         
-        return result
-            
-            
+        return zip(result, result_label)
+               
     def read_all_sensors(self, basic_path:str, bias_head:int, bias_tail:int):
         all_sensor_data = []
 
@@ -114,17 +145,30 @@ class CustomDataset(Dataset):
             return torch.empty(0)
 
         tensor = torch.tensor(combined_rows)
-        print(f"Final tensor shape: {tensor.shape}")
+        if self.debug: print(f"Final tensor shape: {tensor.shape}")
         return tensor
             
-            
-    def __len__(self):
-        return len(self.data)
+    def convert_samples_to_data(self, list_of_samples_with_labels:list, lenght:int):
+        data_list = []
+        label_list = []
+        for _ in range(self.data_from_samples_ratio):
+            for sample, label in list_of_samples_with_labels:
+                random_moment = random.randint(0, sample.shape[0] - lenght)
+                data_list.append(sample[random_moment:random_moment+lenght])
+                label_list.append(self.color_str_to_int(label))
+                
+        data_tensor = torch.stack(data_list)
+        label_tensor = torch.tensor(label_list)
+        
+        return data_tensor, label_tensor
 
-    def __getitem__(self, idx):
-        sample = torch.tensor(self.data[idx], dtype=torch.float32)
-        label = torch.tensor(self.labels[idx], dtype=torch.long)
-        return sample, label
+    def color_str_to_int(self, color):
+        if isinstance(color, np.str_):
+            color = str(color).lower()
+        elif isinstance(color, str):
+            color = color.lower()
+
+        return self.mapping.get(color, 0) 
     
     def read_description_file(self, file_path:str)-> np.ndarray:
         try:
@@ -164,7 +208,7 @@ class CustomDataset(Dataset):
     
 
 if __name__ == "__main__":
-    dataset = CustomDataset("Data\\opis_przejsc.csv", "C:\\Users\\Marcin\\Desktop\\Studia\\IoT\\Data", random_state = 42, mode = DataLoaderType.POCKET, debug=False)
+    dataset = CustomDataset("Data\\opis_przejsc.csv", "C:\\Users\\Marcin\\Desktop\\Studia\\IoT\\Data", data_from_samples_ratio=4, data_lenght = 100 , random_state = 42, mode = DataLoaderType.POCKET, debug=False)
     print("-------------------------------------")
 
     
